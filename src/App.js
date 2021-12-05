855f7c294bea287cb1c90be5957aa85af8806aae
AyoubSmina <01.saayoub@gmail.com>
2021-12-05 18:17:21 +0100

    Day one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import NavBarSection from './Section/NavBarSection';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  useEffect(()=>{
+
+  },[])
+  
+ return (
+    <div style={styles.container}>
+        <NavBarSection />
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  container:{
+    width: '100vw',height: '100vh',
+    padding: 0,margin: 0,
+    backgroundColor: '#ffa'
+  }
+}
---
+++ src/Section/NavBarSection.js
@@ -0,0 +1,206 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import { Search } from '@mui/icons-material';
+import { styled, alpha } from '@mui/material/styles';
+import { InputBase } from '@mui/material';
+
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    // marginLeft: 0,
+    flexGrow:1,
+    borderWidth:1,
+    borderColor:'#00f',
+    borderStyle:'solid',    
+    width: 'auto',
+    // [theme.breakpoints.up('sm')]: {
+    //   marginLeft: theme.spacing(3),
+    //   width: '100%',
+    // },
+  }));
+
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+  
+
+
+
+export default function NavBarSection() {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const DesctopSize = () => (
+        <Box sx={{ flexGrow: 1,display: { xs: 'none', md: 'flex' } }}>
+            <img width="75px" style={styles.LogoLarge} src="./src/logo-udemy.svg" />
+            <Typography
+                variant="subtitle1"
+                component="div"
+                sx={{ marginBlock:'auto',marginInline:'2%' }}
+            >
+                Categorys
+            </Typography>
+            <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                <Badge badgeContent={4} color="error">
+                    <MailIcon />
+                </Badge>
+            </IconButton>
+            <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+            >
+                <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                </Badge>
+            </IconButton>
+            <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+            >
+                <AccountCircle />
+            </IconButton>
+        </Box>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const MobileSize = () => (
+        <Box sx={{ display: { xs: 'flex', md: 'none' }, width: '100vw' }}>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <img width="75px" style={styles.Logo} src="./src/logo-udemy.svg" />
+            <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+            >
+                <SearchIcon />
+            </IconButton>
+            <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+            >
+                <ShoppingCartIcon />
+            </IconButton>
+        </Box>
+    );
+
+    return (
+        <Box sx={{ flexGrow: 1 }} >
+            <AppBar position="static" style={styles.container}>
+                <Toolbar>
+                    <MobileSize />
+                    <DesctopSize />
+                </Toolbar>
+            </AppBar>
+
+        </Box>
+    );
+}
+
+const styles = {
+    container: {
+        padding: '0px 2%',
+        backgroundColor: '#fff',
+        color: "black"
+    }
+    , Logo: {
+        width: '75px',
+        margin: 'auto'
+    }
+    , LogoLarge: {
+        width: '100px',
+    }
+}
